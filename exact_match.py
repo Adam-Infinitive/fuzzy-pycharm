0b389f82f1646bf0d2af5bfa892c4c59932abdf8
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2021-06-11 12:34:46 -0400

    exact match

--- exact_match.py
+++ exact_match.py
@@ -6,12 +6,25 @@ warnings.simplefilter('ignore')
 from csv import reader
 
 # Import Data
-test_data = pd.read_csv('csv-data-main\\sample-data-simple.csv', index_col=False)
+check_data = pd.read_csv('csv-data-main\\sample-data-simple.csv', index_col=False)
 sample_data = pd.read_csv('csv-data-main\\ssn-sample-data.csv', index_col=False)
 fraud_data = pd.read_csv('csv-data-main\\ssn-fraud-sample-data.csv', index_col=False)
 
-def checker()
-for x in range(len(simple_data)):
-    simple_data.columns = ['','','','','','','']
-    print(str(simple_data.loc[[x]].to_string(index=False)))
+def checker(check, sample):
+    check_arr = []
+    verify_arr = []
+    match_arr = []
+    for x in range(len(check)):
+        check.columns = ['','','','','','','']
+        check_arr.append(str(check.loc[[x]].to_string(index=False)))
+    for x in range(len(sample)):
+        sample.columns = ['','','','','','','']
+        verify_arr.append(str(sample.loc[[x]].to_string(index=False)))
 
+    for x in check_arr:
+        if x in verify_arr:
+            print("FOUND MATCH!!")
+            print(x + "\n")
+            match_arr.append(x)
+
+checker(check_data,sample_data)
